34658b0f742869aceadc83ef7c9e52aceccd451c
Alex Coronell <alexcoronell@gmail.com>
2025-02-05 21:19:47 -0500

    tailwind.config.ts completed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,23 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				background: '#1c1c1c',
+				backgroundsecondary: '#212121',
+				foreground: '#eaebdb',
+				primary: '#3fcf8e',
+				red: '#ec5353'
+			},
+			screens: {
+				sm: '576px',
+				md: '768px',
+				lg: '992px',
+				xl: '1200px',
+				'2xl': '1366px',
+				'3xl': '1600px'
+			}
+		}
 	},
 
 	plugins: [typography]
