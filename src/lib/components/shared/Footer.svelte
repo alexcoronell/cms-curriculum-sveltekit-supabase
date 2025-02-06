66b2238a145447ccc907d2b72488da14c4ee075b
Alex Coronell <alexcoronell@gmail.com>
2025-02-05 21:23:17 -0500

    Footer styles completed

--- src/lib/components/shared/Footer.svelte
+++ src/lib/components/shared/Footer.svelte
@@ -5,3 +5,9 @@
 <footer class="Footer">
 	<h6>&copy; {year} - Built by Alex Coronell</h6>
 </footer>
+
+<style>
+	.Footer {
+		@apply text-primary border-primary/50 bg-background fixed bottom-0 left-0 right-0 w-full border-t py-3 text-center text-base font-light;
+	}
+</style>
