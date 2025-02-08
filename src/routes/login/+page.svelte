b8e716d0f342c0bcf76cb0d149bdb957aceefc88
Alex Coronell <alexcoronell@gmail.com>
2025-02-07 19:55:25 -0500

    Basic structure Login form

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import Layout from "../+layout.svelte";
+    import Input from "$lib/components/ui/form/Input.svelte";
+</script>
+
+<Layout>
+    <section class="Login">
+        <div class="Login__container">
+            <form>
+                <div class="Login__inputsArea">
+                    <Input />
+                <Input />
+                </div>
+                <div class="Login__buttonArea">
+                    <button>Button</button>
+                </div>
+            </form>
+        </div>
+    </section>
+</Layout>
+
+<style>
+    
+</style>
