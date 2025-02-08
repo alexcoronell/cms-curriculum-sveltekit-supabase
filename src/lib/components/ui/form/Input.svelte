b5e5da4585bb5f0e907fd7675c6542cfc7cf9fb8
Alex Coronell <alexcoronell@gmail.com>
2025-02-07 19:43:23 -0500

    Basic structure input

---
+++ src/lib/components/ui/form/Input.svelte
@@ -0,0 +1,7 @@
+<div class="form-group">
+    <label for="">
+        <input type="text">
+        <span>Name</span>
+    </label>
+    <p>Message</p>
+</div>
---
+++ src/lib/components/ui/form/Input.test.ts
@@ -0,0 +1,5 @@
+import { describe, it, expect } from 'vitest';
+import { render } from '@testing-library/svelte';
+import '@testing-library/jest-dom';
+
+import Input from './Input.svelte';
