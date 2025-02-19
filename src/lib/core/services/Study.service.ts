32f229a5048f1a22d051ae972f1f50e2a72cdce2
Alex Coronell <alexcoronell@gmail.com>
2025-02-18 19:50:07 -0500

    Study Service completed not tested

---
+++ src/lib/core/services/Study.service.ts
@@ -0,0 +1,22 @@
+import { createClient } from '$supabase/supabaseClient';
+
+/* Models */
+import type { Study } from '$models/Study.interface';
+
+class StudyService {
+	private supabase = createClient();
+	private table = 'studies';
+
+	getAll = async (): Promise<Study[]> => {
+		const { data, error } = await this.supabase
+			.from(this.table)
+			.select('*')
+			.order('current', { ascending: false })
+			.order('year', { ascending: false })
+			.order('month', { ascending: false });
+		if (error) throw new Error(error.message);
+		return data as Study[];
+	};
+}
+
+export default StudyService;
