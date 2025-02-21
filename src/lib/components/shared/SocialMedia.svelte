96fdcfde38357e25573252340d413eb8d46d3c1b
Alex Coronell <alexcoronell@gmail.com>
2025-02-20 19:07:36 -0500

    Contact page basic layout

---
+++ src/lib/components/shared/SocialMedia.svelte
@@ -0,0 +1,1 @@
+<h3>SocialMedia</h3>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,1 +1,19 @@
-<h1>Contact Page</h1>
+<script lang="ts">
+    import PublicPagesLayout from "$components/shared/PublicPagesLayout.svelte";
+    import ContactForm from "$components/shared/ContactForm.svelte";
+    import SocialMedia from "$components/shared/SocialMedia.svelte";
+    const title = 'Contact';
+</script>
+
+<PublicPagesLayout {title}>
+    <div class="Contact">
+        <ContactForm />
+        <SocialMedia />
+    </div>
+</PublicPagesLayout>
+
+<style>
+    .Contact {
+        @apply lg:grid lg:grid-cols-2 gap-x-10 h-full items-center;
+    }
+</style>
