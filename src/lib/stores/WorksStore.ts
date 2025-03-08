44df192fd54524e4fcbf78cb85036c852839cb1f
Alex Coronell <alexcoronell@gmail.com>
2025-03-07 19:45:08 -0500

    Calculate total pages completed

--- src/lib/stores/WorksStore.ts
+++ src/lib/stores/WorksStore.ts
@@ -1,4 +1,4 @@
-import { writable } from 'svelte/store';
+import { writable, get } from 'svelte/store';
 
 /* Interfaces */
 import type { Result } from '$interfaces/Result.interface';
@@ -9,38 +9,43 @@ import type { Work } from '$models/Work.interface';
 /* Services */
 import WorkService from '$services/Work.service';
 
+const createWorkStore = () => {
+	const { subscribe, set, update } = writable<Result<Work>>({
+		data: [],
+		count: 0,
+		loading: false,
+		error: false,
+		message: ''
+	});
+	const pages = writable<number>(0);
+	const currentPage = writable<number>(1);
+	let limit = writable<number>(10);
 
-const createWorkStore =()=>{
-    const { subscribe, set, update } = writable<Result<Work>>({
-        data: [],
-        count: 0,
-        loading: false,
-        error: false,
-        message: ''
-    });
-    
-    const service = new WorkService()
+	const service = new WorkService();
 
-    return {
-        subscribe,
-        loadWorks: async(from: number = 0, to: number = 10) => {
-            set({
-                data: [],
-                count: 0,
-                loading: true,
-                error: false,
-                message: ''
-            })
-            const { data, count, error, message } = await service.getAllSimple(from, to);
-            set({
-                data,
-                count,
-                loading: false,
-                error,
-                message
-            })
-        }
-    }
-}
+	return {
+		subscribe,
+		loadWorks: async (from: number = 0, to: number = 10) => {
+			set({
+				data: [],
+				count: 0,
+				loading: true,
+				error: false,
+				message: ''
+			});
+			const { data, count, error, message } = await service.getAllSimple(from, to);
+			const limitValue = get(limit)
+			const totalPages = Math.ceil(count / limitValue);
+			pages.set(totalPages);
+			set({
+				data,
+				count,
+				loading: false,
+				error,
+				message
+			});
+		}
+	};
+};
 
 export const WorksStore = createWorkStore();
